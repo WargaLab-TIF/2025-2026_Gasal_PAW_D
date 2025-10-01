affd6d0f553afaf4d608c3f0ae1da9ec9fd42bc3
Milktearoses <dmrelk356@gmail.com>
2025-10-01 18:46:27 +0700

    nugas lagi

--- TM-1/24-059_Putra_Fajar_Suhardi/index.php
+++ TM-1/24-059_Putra_Fajar_Suhardi/index.php
@@ -1,6 +1,16 @@
 <?php
 	if (isset($_POST['batas'])){
 		$batas = $_POST['batas'];
-		echo "Batas ukuran : $batas";
+		echo "Batas ukuran : $batas<br>";
+	}
+
+	if (isset($_POST['warna'])){
+		$warna = $_POST['warna'];
+		echo "Warna yang dipilih : $warna<br>";
+	}
+
+	if (isset($_POST['pola'])){
+		$pola = $_POST['pola'];
+		echo "Pola yang dipilih : $pola<br>";
 	}
 ?>
