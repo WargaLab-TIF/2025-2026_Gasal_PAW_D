e946bef454fb888f97073ec82cb4432372d48ae2
labibuddin18 <tokomanfaat6@gmail.com>
2025-10-03 20:50:43 +0700

    menyelesaikan tugas TM1

---
+++ TM-1/PAW2025-1_D_24-152_Ahmad_Labib_Arifudin/index.php
@@ -0,0 +1,58 @@
+<?php include 'prosesData.php' ?>
+<!DOCTYPE html>  
+<html lang="id">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Tabel modulus(baris % kolom)</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<form method="POST">
+		<table class="table1">
+			<tr>
+				<th><label for="ukuran">Ukuran :</label></th>
+				<td><input type="number" name="ukuran" min="1" required></td>
+			</tr>
+			<tr>
+				<th><label for="aturan">Aturan :</label></th>
+				<td>
+					<select name="aturan" required>
+						<option value="">--pilih--</option>
+						<option value="kubik">Bilangan Kubik</option>
+						<option value="batas">Perbatasan</option>
+						<option value="prima">Bilangan prima</option>
+						<option value="diagonal">Arsiran Diagonal</option>
+					</select>
+				</td>
+			</tr>
+			<tr>
+				<th>Warna :</th>
+				<td>
+					<select name="warna" required>
+						<option value="">--pilih--</option>
+						<option value="red">Red</option>
+						<option value="yellow">Yellow</option>
+						<option value="green">Green</option>
+						<option value="pink">Pink</option>
+					</select>	
+				</td>
+			</tr>
+			<tr>
+				<td><button type="submit">submit</button></td>
+			</tr>
+		</table>
+	</form>
+	<table class="table2">
+		<?php
+		if (isset($_POST['ukuran'])) {
+			$ukuran=$_POST['ukuran'];
+			$aturan=$_POST['aturan'];
+			$warna=$_POST['warna'];
+			fungsi($ukuran,$aturan,$warna);
+		}
+
+		 ?>
+	</table>
+</body>
+</html>
