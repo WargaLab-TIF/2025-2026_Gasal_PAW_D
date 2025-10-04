2dae45a5ff3323008957754ad93e9c9389b1ebf5
Fu4di4chmd <achmadsaifulfuadifuad@gmail.com>
2025-10-04 14:58:33 +0700

    TM1_24-012

---
+++ TM-1/PAW2025-1_D_24-012_Achmad_Saiful_Fuadi/index.php
@@ -0,0 +1,50 @@
+<?php
+require_once "functions.php";
+
+$batas = isset($_POST['batas']) ? (int)$_POST['batas']:"";
+?>
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas Mingguan 1 - NIM Genap</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>tabel hasil modulus</h1>
+    <form method="post">
+        <label for="batas">Batas ukuran:</label>
+        <input type="number" name="batas" id="batas" min="1" value="<?php echo $batas;?>" required><br><br>
+
+        <label for="filter">Aturan Filter:</label>
+        <select name="filter" id="filter">
+            <option value="kubik">Bilangan Kubik</option>
+            <option value="perbatasan">Pola Perbatasan</option>
+            <option value="prima">Bilangan Prima</option>
+            <option value="diagonal">Arsiran Diagonal (kiri atas → kanan bawah)</option>
+        </select><br><br>
+
+        <label for="warna">Warna Filter:</label>
+        <select name="warna" id="warna">
+            <option value="yellow">Kuning</option>
+            <option value="lightblue">Biru Muda</option>
+            <option value="lightgreen">Hijau Muda</option>
+        </select><br><br>
+
+        <button type="submit">Tampilkan</button>
+    </form>
+
+    <hr>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        $batas  = intval($_POST["batas"]);
+        $filter = $_POST["filter"];
+        $warna  = $_POST["warna"];
+
+        tampilkanTabelModulus($batas, $filter, $warna);
+    }
+    ?>
+</body>
+</html>
