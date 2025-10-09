87e00b17b8a4f67e3c38687357c29d9536c594f1
muhammadalimurtadho05 <muhammadalimurtadho2209051@gmail.com>
2025-10-09 23:13:23 +0700

    Upload TM 2

--- TM-2/PAW2025-1_D_24-119_Muhammad_Ali_Murtadho/index.php
+++ TM-2/PAW2025-1_D_24-119_Muhammad_Ali_Murtadho/index.php
@@ -11,7 +11,7 @@ if (isset($_POST['submit'])) {
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Form Pembukaan Rekening</title>
+	<title>Form Pendaftaran M-Banking</title>
 	<link rel="stylesheet" href="style.css">
 </head>
 <body>
