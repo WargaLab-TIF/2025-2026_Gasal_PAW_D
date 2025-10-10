c039bc67f1e1884a3d307c4390c047322ff78ccc
Achmad Junaidi S <acjusy@gmail.com>
2025-10-10 20:51:48 +0700

    update

---
+++ TM-2/PAW2025-1_D_24-117_Achmad_Junaidi_syafaruddin/index.php
@@ -0,0 +1,52 @@
+<?php 
+	
+	require 'validasi.php';
+
+
+	if (isset($_POST['submit'])) {
+		$nama = $_POST['nama'];
+		$no = $_POST['no'];
+		$password = $_POST['password'];
+		$nik = $_POST['nik'];
+		$pin = $_POST['pin'];
+
+		$eror = []; 
+
+		valNama($nama, $eror);
+		no_tlp($no,$eror);
+		pass($password,$eror);
+		nik($nik,$eror);
+		pin($pin,$eror);
+
+			
+	}
+
+ ?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>form aktivasi</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="form-container">
+		<h1>aktivasi rekening</h1>
+		<form action="" method="post">
+			<fieldset>
+				<?php if (isset($_POST['submit']) && count($eror) == 0): 
+				 ?>
+				 <h1>berhasil aktivasi</h1> 
+				 <?php require 'success.php'; ?>
+				<?php else: ?>
+				<?php require 'form.php'; ?>
+			<?php endif; ?>
+			</fieldset>
+		</form>
+	</div>
+</body>
+</html>
---
+++ TM-2/PAW2025-1_D_24-117_Achmad_Junaidi_syafaruddin/success.php
@@ -0,0 +1,24 @@
+
+<table>
+	<tr>
+		<td>Nama</td>
+		<td><?php echo $_POST['nama']?></td>
+	</tr>
+	<tr>
+		<td>Password</td>
+		<td><?php echo $_POST['password']?></td>
+	</tr>
+	<tr>
+		<td>no telepon</td>
+		<td><?php echo $_POST['no']?></td>
+	</tr>
+	<tr>
+		<td>NIK</td>
+		<td><?php echo $_POST['nik']?></td>
+	</tr>
+	<tr>
+		<td>PIN</td>
+		<td><?php echo $_POST['pin']?></td>
+	</tr>
+
+</table>
